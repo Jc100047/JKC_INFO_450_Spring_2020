6e170f0bc5c5d263847c0814aff017f0e7241938
Jc100047 <59657738+Jc100047@users.noreply.github.com>
2020-03-05 09:28:27 -0500

    Update program1.cpp

--- week7/program1.cpp
+++ week7/program1.cpp
@@ -37,7 +37,7 @@ string reverse(string *s1)
     for(int i = 0; i < length / 2; i++)             // The for loop swaps the letters of the beginning and ending of the string until it gets to the middle.
     {                                               //
         swap(s1->at(i), s1->at(length - i - 1));    // If it didn't stop at the middle of the string and instead stopped at the end of the string, everything that was
-    }                                               // swapped in the first half will be swapped back and the string will be the same as the string we started with.
+    }                                               // swapped in the first half will be swapped back and the string will be the same as what we started with.
 
     return *s1;
 
