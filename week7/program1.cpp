29f27b6b56ca61d4dfbc4c04d35c9aa69f1a6150
Jc100047 <59657738+Jc100047@users.noreply.github.com>
2020-03-05 08:44:22 -0500

    Update program1.cpp

--- week7/program1.cpp
+++ week7/program1.cpp
@@ -32,10 +32,10 @@ string reverse(string s1)
 {
     int length = s1.length();
 
-    for(int i = 0; i < length / 2; i++)
-    {
-        swap(s1[i], s1[length - i - 1]);
-    }
+    for(int i = 0; i < length / 2; i++)     // The for loop swaps the letters of the beginning and ending of the string until it gets to the middle.
+    {                                       //
+        swap(s1[i], s1[length - i - 1]);    // If it didn't stop at the middle of the string and instead stopped at the end of the string, everything that was  
+    }                                       // swapped in the first half will be swapped back and the string will be the same as the string we started with.
 
     return s1;
 
