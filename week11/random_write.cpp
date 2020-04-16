a3d453ed46473e9598ea0273dfe48dfc3b5d4c75
Jc100047 <59657738+Jc100047@users.noreply.github.com>
2020-04-15 20:35:34 -0400

    Add files via upload

---
+++ week11/random_write.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+int main()
+{
+    ofstream out("randoms.txt");
+    if(!out)
+    {
+        cout << "Cannot open file." << endl;
+        return 1;
+    }
+
+    srand (time(NULL));
+    int randNum;
+
+    for(int i = 0; i < 1000; i++)
+    {
+        randNum = rand() % 2000 + (-1000);
+
+        out << randNum << endl;
+    }
+
+    out.close();
+
+    return 0;
+
+}
+
